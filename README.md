b3c862eaaa0b0512cee759e7fd2eb47493d42cff
Joe Julian <me@joejulian.name>
2012-10-22 15:11:02 -0700

    Initial Check In

--- README.md
+++ README.md
@@ -1,4 +1,111 @@
-ufopilot
-========
+ufopilot 
+======== 
+ 
+UfoPilot is an HTML5/Javascript tool for managing Gluster Unified File and Object (UFO) Storage. 
+ 
+Installation
+============
+Install GlusterFS according to the [QuickStart Guide](http://www.gluster.org/community/documentation/index.php/QuickStart)
 
-HTML5/Javascript tool for managing Gluster Unified File and Object (UFO) Storage.
+The rest of these instructions will assume you used the volume named gv0 as in the QuickStart Guide
+
+Install the swift components:
+    yum install 'glusterfs-swift*' memcached
+    
+Add some middleware to /etc/swift/proxy-server.conf
+
+Change the pipeline to read:
+
+    pipeline = healthcheck cache tempauth staticweb tempurl formpost proxy-server
+    
+Add the following sections:
+
+    [filter:cache]
+    use = egg:swift#memcache
+    memcache_servers = 127.0.0.1:11211
+    
+    [filter:staticweb]
+    use = egg:swift#staticweb
+    
+    [filter:tempurl]
+    use = egg:swift#tempurl
+    
+    [filter:formpost]
+    use = egg:swift#formpost
+
+Set up your user(s) by adding them to the [filter:tempauth] section. For this example, the username will be "tommy"
+and the password will be "demo". Note gv0 in there which is the volume we created earlier:
+
+    user_gv0_tommy=demo .admin
+
+For production, you should probably enable encryption by setting the cert\_file and key\_file after creating the 
+appropriate files with your certificate provider (OPTIONAL):
+
+    bind_port = 443
+    cert_file = /etc/swift/cert.crt
+    key_file = /etc/swift/cert.key
+
+These instructions will continue using unencrypted connections.
+
+Start your new services
+
+    service memcached start
+    service gluster-swift-object start
+    service gluster-swift-container start
+    service gluster-swift-account start
+    service gluster-swift-proxy start
+
+Retrieve this package and it's dependency
+
+    git clone git://github.com/joejulian/ufopilot.git
+    wget https://github.com/downloads/Caligatio/jsSHA/jsSHA-1.31.tar.bz2
+    cd ufopilot
+    wget -O - https://github.com/downloads/Caligatio/jsSHA/jsSHA-1.31.tar.bz2 | tar xjv -C /tmp -f - jsSHA/src/sha.js
+    mv /tmp/jsSHA/src/sha.js .
+    rm -rf /tmp/jsSHA
+
+Set some shell variables to make the rest of this more generic and save you copy/pasters some time
+
+    $host=http://localhost:8080
+    $user=tommy
+    $pass=demo
+    $volume=gv0
+    $webcon=app
+
+Create a container to be the web app container and set the permissions and index page
+
+    swift -A $host/auth/v1.0 -U $volume:$user -K $pass post $webcon
+    swift -A $host/auth/v1.0 -U $volume:$user -K $pass post $webcon -m 'web-index:index.html'
+    swift -A $host/auth/v1.0 -U $volume:$user -K $pass post $webcon -r '.r:*'
+    
+Add a key for tempurl signing
+    
+    swift -A $host/auth/v1.0 -U $volume:$user -K $pass post -m 'tempurlkey'
+    
+Upload these files to your web app container
+
+    swift -A $host/auth/v1.0 -U $volume:$user -K $pass upload $webcon *
+
+Open your new app in your browser
+    [My New Swift App Server](http://localhost:8080/v1/AUTH_gv0/app/)
+
+
+License 
+========
+Copyright (c) 2012 Joe Julian, All Rights Reserved 
+ 
+This file is part of ufopilot 
+ 
+ufopilot is free software: you can redistribute it and/or modify 
+it under the terms of the GNU General Public License as published by 
+the Free Software Foundation, either version 3 of the License, or 
+(at your option) any later version. 
+ 
+ufopilot is distributed in the hope that it will be useful, 
+but WITHOUT ANY WARRANTY; without even the implied warranty of 
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the 
+GNU General Public License for more details. 
+ 
+You should have received a copy of the GNU General Public License 
+along with ufopilot.  If not, see <http://www.gnu.org/licenses/>. 
+
---
+++ swiftapi.js
@@ -0,0 +1,352 @@
+/*  Copyright (c) 2012 Joe Julian, All Rights Reserved
+ *
+ *  This file is part of ufopilot
+ *
+ *  ufopilot is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  ufopilot is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with ufopilot.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+var swiftdemo = {
+    authkey: "",
+    baseurl: "",
+    volume:  "ufodemo",
+    containers: {},
+    auth: function (form) {
+        var username = form.username.value;
+        var password = form.password.value;
+        this.authorize(username, password);
+        return false;
+    },
+    reauthorize: function (stage) {
+        if(stage == 0) {
+            if(this.authkey) {
+                this.checkkey(this.authkey,stage);
+                return false;
+            } else {
+                stage = 1;
+            }
+        }
+        if(stage == 1) {
+            if(supports_html5_storage()) {
+                var authkey = localStorage["swiftdemo.authkey"];
+                if(authkey) {
+                    this.checkkey(authkey,stage);
+                    return false;
+                }
+            }
+        }
+        swiftdemo.list.display(false);
+        swiftdemo.upload.display(false);
+        swiftdemo.container.display(false);
+        swiftdemo.authinfo.display(true);
+    },
+    checkkey: function (key, stage) {
+        if(key) {
+            $.ajax({
+                type: "HEAD",
+                beforeSend: function (request) {
+                    request.setRequestHeader("X-Auth-Token", key);
+                },
+                url: this.baseurl + "/v1/AUTH_" + this.volume,
+                processData: false,
+                success: function (data, status, request) {
+                    swiftdemo.authkey = key;
+                    if(supports_html5_storage()) {
+                        localStorage['swiftdemo.authkey'] = key;
+                    }
+                    swiftdemo.list.display(true);
+                    swiftdemo.authinfo.display(false);
+                },
+                error: function (data, status, request) {
+                    swiftdemo.reauthorize(stage + 1);
+                },
+            });
+        }
+    },
+    authorize: function (username, password) {
+        $.ajax({
+            type: "GET",
+            beforeSend: function (request) {
+                request.setRequestHeader("X-Storage-User", swiftdemo.volume + ":" + username);
+                request.setRequestHeader("X-Storage-Pass", password);
+            },
+            url: this.baseurl + "/auth/v1.0",
+            processData: false,
+            success: function (data, status, request) {
+                swiftdemo.authkey = request.getResponseHeader("X-Auth-Token");
+                localStorage['swiftdemo.authkey'] = swiftdemo.authkey;
+                swiftdemo.list.display(true);
+                swiftdemo.authinfo.display(false);
+            },
+            error: function() {
+                alert("Invalid Credentials");
+            }
+        });
+    },
+    download: function(target) {
+        target = "/v1/AUTH_" + swiftdemo.volume + "/" + target
+        var d = new Date();
+        var unixtime = Math.ceil(d.valueOf() / 1000 + 600).toString();
+        var hmac_body = ["GET", unixtime, target].join("\n");
+        var shaObj = new jsSHA(hmac_body, "ASCII");
+        var sig = shaObj.getHMAC("demotempurlkey", "ASCII", "SHA-1", "HEX");
+        var iframe = document.getElementById("hiddenDownloader");
+        if ( iframe === null ) {
+            iframe = document.createElement('iframe');
+            iframe.id = "hiddenDownloader";
+            iframe.style.visibility = "hidden";
+            document.body.appendChild(iframe);
+        }
+        iframe.src = swiftdemo.baseurl + target + '?temp_url_sig=' + sig +
+            '&temp_url_expires=' + unixtime;
+        return false;
+    },
+    logout: function() {
+        localStorage["swiftdemo.authkey"] = null;
+        this.authkey = null;
+        swiftdemo.list.display(false);
+        swiftdemo.upload.display(false);
+        swiftdemo.container.display(false);
+        swiftdemo.authinfo.display(true);
+        return false;
+    },
+    authinfo: {
+        display: function(bool) {
+            var thisdiv = document.getElementById("authinfo");
+            if (bool) {
+                thisdiv.style.display = "inherit";
+            } else {
+                thisdiv.style.display = "none";
+            }
+        }
+    },
+    list: {
+        display: function(bool) {
+            if (this.authkey != "") {
+                var thisdiv = document.getElementById("list");
+                if (bool) {
+                    // Load the list here
+                    this._getContainers();
+                    thisdiv.style.display = "inherit";
+                } else {
+                    thisdiv.style.display = "none";
+                }
+            } else {
+                swiftdemo.authinfo.display(true);
+            }
+        },
+        _getContainers: function() {
+            $.ajax({
+                type: "GET",
+                dataType: 'json',
+                url: swiftdemo.baseurl + "/v1/AUTH_" + swiftdemo.volume,
+                beforeSend: function (request) {
+                    request.setRequestHeader("X-Auth-Token", swiftdemo.authkey);
+                    request.setRequestHeader("Accept", "application/json");
+                },
+                success: function (data) {
+                    swiftdemo.containers = data;
+                    swiftdemo.list.viewContainers();
+                },
+                error: function (data) {
+                    swiftdemo.list.display(false);
+                    swiftdemo.authinfo.display(true);
+                },
+            });
+        },
+        viewContainers: function() {
+            var items = [];
+            var selected = "";
+            $.each(swiftdemo.containers, function(key, val) {
+                items.push('<li id=li_' + val['name'] + '>' + swiftdemo.list.containerItem( key, val ) + '</li>');
+            });
+
+            $('#filelist').replaceWith($('<fieldset/>', {
+                'id': 'filelist',
+                html: $('<ul/>', {
+                    'class': 'containers',
+                    html: items.join('')
+                }),
+            }))
+            $.each(swiftdemo.containers, function(key, val) {
+                var obj = document.getElementById(val['name']);
+                if(swiftdemo.container.selected[val['name']]) {
+                    obj.checked = true
+                } else {
+                    obj.checked = false
+                }
+                swiftdemo.container.select(obj);
+            });
+        },
+        containerItem: function(key, val) {
+            if(swiftdemo.container.selected[val['name']]) {
+                selected = " checked";
+            } else {
+                selected = "";
+            }
+            return '<label for="' + val['name'] + '">' + val['name'] + 
+                '</label><input type="checkbox" id="' + val['name'] + 
+                '" onchange="swiftdemo.container.select(this)"'+ selected + '>';
+        },
+        refresh: function() {
+            this._getContainers();
+            return false;
+        },
+    },
+    container: {
+        contents: {},
+        selected: {},
+        create: function(form) {
+            var container = form.container.value;
+            $.ajax({
+                type: "PUT",
+                processData: false,
+                url: swiftdemo.baseurl + "/v1/AUTH_" + swiftdemo.volume + "/" + container,
+                beforeSend: function (request) {
+                    request.setRequestHeader("X-Auth-Token", swiftdemo.authkey);
+                    request.setRequestHeader("Accept", "application/json");
+                },
+                success: function () {
+                    swiftdemo.list.display(true);
+                    swiftdemo.container.display(false);
+                },
+                error: function (data) {
+                    swiftdemo.authinfo.display(true);
+                    swiftdemo.container.display(false);
+                },
+            });
+            return false;
+        },
+        display: function(bool) {
+            if (this.authkey != "") {
+                var thisdiv = document.getElementById("create");
+                if (bool) {
+                    thisdiv.style.display = "inherit";
+                    swiftdemo.list.display(false);
+                } else {
+                    thisdiv.style.display = "none";
+                }
+            } else {
+                swiftdemo.authinfo.display(true);
+            }
+            return false;
+        },
+        select: function(container) {
+            var checked = container.checked
+            var id = container.id
+            if(container.checked) {
+                var thisli = document.getElementById("li_" + container.id);
+                thisli.style.listStyleType = "circle";
+                this._getObjects(container.id);
+                this.selected[container.id] = true;
+            } else {
+                var thisli = document.getElementById("li_" + container.id);
+                thisli.style.listStyleType = "disc";
+                $('#files_' + container.id).replaceWith("");
+                this.selected[container.id] = false;
+            }
+        },
+        _getObjects: function(container) {
+            $.ajax({
+                type: "GET",
+                dataType: 'json',
+                url: swiftdemo.baseurl + "/v1/AUTH_" + swiftdemo.volume + "/" + container,
+                beforeSend: function (request) {
+                    request.setRequestHeader("X-Auth-Token", swiftdemo.authkey);
+                    request.setRequestHeader("Accept", "application/json");
+                },
+                success: function (data) {
+                    swiftdemo.container.contents[container] = data;
+                    swiftdemo.container.view(container);
+                },
+                error: function (data) {
+                    swiftdemo.authinfo.display(true);
+                    swiftdemo.list.display(false);
+                },
+            });
+        },
+        view: function(container) {
+            var items = [];
+            $.each(swiftdemo.container.contents[container], function(key, val) {
+                if(val.content_type != "application/directory") {
+                    items.push('<li id="li_' + container + '/' + val['name'] + '">' + 
+                        '<a href="#" onMouseDown="return swiftdemo.download(\'' + container + '/' + val['name'] +
+                        '\')">' + val['name'] + '</a></li>');
+                } else {
+                    items.push('<li id="li_' + container + '/' + val['name'] + '" style="list-style-type: disc">' + 
+                        val['name'] + '</li>');
+                }
+            });
+
+            $('#li_' + container).append($('<ul/>', {
+                'id'   : 'files_' + container,
+                'class': 'files',
+                html   : items.join('')
+            }));
+        },
+    },
+    upload: {
+        display: function(bool) {
+            if (this.authkey != "") {
+                var thisdiv = document.getElementById("upload");
+                if (bool) {
+                    thisdiv.style.display = "inherit";
+                    swiftdemo.list.display(false);
+                    this.startForm();
+                } else {
+                    thisdiv.style.display = "none";
+                }
+            } else {
+                swiftdemo.authinfo.display(true);
+            }
+            return false;
+        },
+        startForm: function() {
+            var items = [];
+            var foo = Object.keys(swiftdemo.container.contents)
+            $.each(swiftdemo.containers, function(key, val) {
+                items.push('<option>' + val['name'] + '</option>');
+            });
+            $('#upload_form_container').html(items.join());
+            this.selectContainer();
+        },
+        selectContainer: function() {
+            var target = "/v1/AUTH_" + swiftdemo.volume + "/" + $('#upload_form_container').val();
+            alert("Target is " + target);
+            $('#upload_form').attr("action",target);
+            var redirect = window.location.pathname;
+            $('#upload_form_redirect').val(redirect);
+            var max_file_size = $('#upload_form_size').val();
+            var max_file_count = $('#upload_form_count').val();
+            var d = new Date();
+            var expires = Math.ceil(d.valueOf() / 1000 + 3600).toString();
+            $('#upload_form_expire').val(expires);
+            var hmac_body = [target, redirect, max_file_size, max_file_count, expires ].join("\n");
+            var shaObj = new jsSHA(hmac_body, "ASCII");
+            var sig = shaObj.getHMAC("demotempurlkey", "ASCII", "SHA-1", "HEX");
+            $('#upload_form_sig').val(sig);
+        },
+    },
+}
+
+function supports_html5_storage() {
+    try {
+        return 'localStorage' in window && window['localStorage'] !== null;
+    } catch (e) {
+        return false;
+    }
+}
+
+$(document).ready(function() {
+    swiftdemo.reauthorize(0);
+});
